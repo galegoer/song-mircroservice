29569f75907ddb44bae7a26fdb1e07eac335452a
liwill13 <willl.li@mail.utoronto.ca>
2020-03-26 23:58:48 -0400

    [DEV-1.3] implemented addSong basics

--- starter_code/song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ starter_code/song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -1,11 +1,14 @@
 package com.csc301.songmicroservice;
 
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import com.mongodb.BasicDBObject;
 
 @Repository
 public class SongDalImpl implements SongDal {
@@ -19,8 +22,28 @@ public class SongDalImpl implements SongDal {
 
 	@Override
 	public DbQueryStatus addSong(Song songToAdd) {
-		// TODO Auto-generated method stub
-		return null;
+		DbQueryStatus rtn;
+		
+		Document document = new Document(); //BasicDbobject doc = new basicDebobeject()
+		document.put("songName", songToAdd.getSongName());
+		document.put("songArtistFullName", songToAdd.getSongArtistFullName());
+		document.put("songAlbum", songToAdd.getSongAlbum());
+		document.put("songAmountFavourites", songToAdd.getSongAmountFavourites());
+		document.put("id", songToAdd._id);
+
+//may be useful for something else later
+//		BasicDBObject documentDetail = new BasicDBObject();
+//		documentDetail.put("addressLine1", "Sweet Home");
+//		documentDetail.put("addressLine2", "Karol Bagh");
+//		documentDetail.put("addressLine3", "New Delhi, India");
+//		 
+//		document.put("address", documentDetail);
+		 
+		db.getCollection("songs").insertOne(document);
+		
+		rtn = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		rtn.setData(document); //unless its "data" : all the puts
+		return rtn;
 	}
 
 	@Override
