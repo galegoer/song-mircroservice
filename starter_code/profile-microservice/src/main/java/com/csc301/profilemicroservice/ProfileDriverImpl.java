9d7775f4bb514843deec7a1e3d5d6e6451ed7c60
liwill13 <willl.li@mail.utoronto.ca>
2020-03-28 19:43:41 -0400

    [Dev-2.2] implemented followFriend

--- starter_code/profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
+++ starter_code/profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileDriverImpl.java
@@ -1,5 +1,7 @@
 package com.csc301.profilemicroservice;
 
+import static org.neo4j.driver.v1.Values.parameters;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +13,10 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 
 import org.springframework.stereotype.Repository;
+
+import com.csc301.songmicroservice.DbQueryExecResult;
+import com.csc301.songmicroservice.DbQueryStatus;
+
 import org.neo4j.driver.v1.Transaction;
 
 @Repository
@@ -46,8 +52,39 @@ public class ProfileDriverImpl implements ProfileDriver {
 
 	@Override
 	public DbQueryStatus followFriend(String userName, String frndUserName) {
+		try (Session session = ProfileMicroserviceApplication.driver.session()) {
+			try (Transaction trans = session.beginTransaction()) {
+				
+				//check if userName and frnUserName profiles exist
+				StatementResult u1 = trans.run("MATCH (a:profile) WHERE a.userName = $uname RETURN a.userName", parameters("uname", userName)); 
+				StatementResult u2 = trans.run("MATCH (a:profile) WHERE a.userName = $uname RETURN a.userName", parameters("uname", frndUserName)); 
+        		if(!u1.hasNext())  //if not exist
+        			return new DbQueryStatus("userName not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+        		
+        		if(!u2.hasNext())  //if not exist
+            		return new DbQueryStatus("frndUserName not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+            	
+            	//check if relationship exists
+            	StatementResult relationshipCheck = trans.run("MATCH (:profile { userName: {x} })-[r:follows]->(:profile { userName: {y}}) RETURN type(r)", parameters("x", userName, "y", frndUserName));
+            	if(relationshipCheck.hasNext()) {
+            		return new DbQueryStatus("relationship already exists", DbQueryExecResult.QUERY_ERROR_GENERIC);
+            	}
+        		
+            	//add relationship
+            	StatementResult result = trans.run("MATCH (a:profile), (b:profile) "
+						+ "WHERE a.userName = $uname AND b.userName = $frndname "
+						+ "CREATE (a)-[r:follows]->(b) "
+						+ "RETURN type(r)", parameters("uname", userName, "frndname", frndUserName));
+        		trans.success();  // Mark this write as successful.
+        		return new DbQueryStatus("relationship successfully added", DbQueryExecResult.QUERY_OK);
+        		
+        		 
+        	}
+			catch(Exception e) {
+				return new DbQueryStatus("FAILED TO RUN TRANSACTION", DbQueryExecResult.QUERY_ERROR_GENERIC);
+			}
 		
-		return null;
+		}
 	}
 
 	@Override
